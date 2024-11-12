ccfd732937a9aed2d3ca13f99ccdc6130f4e2f36
Lakshyajeet Jalal <lakshyajeetjalal@gmail.com>
2024-11-12 17:48:27 +0530

    more social links

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,14 @@
 interface SocialLinks {
+	discord: boolean | string;
 	facebook: boolean | string;
 	github: boolean | string;
 	instagram: boolean | string;
+	linkedin: boolean | string;
+	linktree: boolean | string;
+	telegram: boolean | string;
 	twitter: boolean | string;
 	whatsapp: boolean | string;
+	youtube: boolean | string;
 }
 
 interface Config {
